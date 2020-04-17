a2ff2a192a242ddadcfa90e49117ca14d5a098c3
PixelRayn <david.kowalk@gmail.com>
2020-04-17 11:06:59 +0200

    Fixed import

--- src/__init__.py
+++ src/__init__.py
@@ -2,4 +2,4 @@ from function_synthesizer.function_synthesizer import solve
 from function_synthesizer.function_synthesizer import solve_mixed
 from function_synthesizer.function_synthesizer import to_str
 from function_synthesizer.function_synthesizer import calculate
-from function_synthesizer.function_synthesizer import read_points
+from function_synthesizer.function_synthesizer import read
